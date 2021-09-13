bc4395d5afe95b61806d9f940ddd6229b7ab3d87
YohiroW <yohiro36@foxmail.com>
2021-09-13 23:52:51 +0800

    Initialize plugin shader path

--- Poseidon/Source/Poseidon/Private/Poseidon.cpp
+++ Poseidon/Source/Poseidon/Private/Poseidon.cpp
@@ -1,12 +1,21 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "Poseidon.h"
+#include "Misc/Paths.h"
+#include "Interfaces/IPluginManager.h"
 
 #define LOCTEXT_NAMESPACE "FPoseidonModule"
 
 void FPoseidonModule::StartupModule()
 {
 	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+
+	FString shaderPath;
+	if (TSharedPtr<IPlugin> poseidon = IPluginManager::Get().FindPlugin(TEXT("Poseidon")))
+	{
+		shaderPath = FPaths::Combine(poseidon->GetBaseDir(), TEXT("Shaders"));
+	}
+	AddShaderSourceDirectoryMapping(TEXT("/Plugin/Poseidon"), shaderPath);
 }
 
 void FPoseidonModule::ShutdownModule()
