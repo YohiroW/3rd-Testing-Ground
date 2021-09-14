f64c0843bb50bf3d9fccc89a4c94d38607b37ae3
YohiroW <yohiro36@foxmail.com>
2021-09-14 23:58:17 +0800

    Create global shader Poseidon Shader

--- Poseidon/Source/Poseidon/Public/PoseidonComponent.h
+++ Poseidon/Source/Poseidon/Public/PoseidonComponent.h
@@ -12,6 +12,7 @@ class FRHITexture;
 class FRHIUnorderedAccessView;
 class FRHICommandListImmediate;
 
+// As uniform buffer
 USTRUCT(BlueprintType)
 struct FOceanInfo
 {
@@ -23,22 +24,46 @@ struct FOceanInfo
 	FVector4 OceanTime;
 };
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(HideCategories = (Object, LOD, Physics, Collision), editinlinenew, meta = (BlueprintSpawnableComponent), ClassGroup = Rendering, DisplayName = "PoseidonComponent")
 class POSEIDON_API UPoseidonComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
 public:	
 	// Sets default values for this component's properties
-	UPoseidonComponent();
+	UPoseidonComponent(const FObjectInitializer& ObjectInitializer);
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
 public:	
+	//
+	virtual void OnRegister() override;
+
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PoseidonComponent")
+	UTextureRenderTarget2D* OutputRenderTarget2D;
+
+	// Specified uniform buffer data
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PoseidonComponent")
+	FOceanInfo OceanInfoBuffer;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "PoseidonComponent")
+	int32 TargetSize;
+
+	ETextureRenderTargetFormat RenderTargetFormat;
+
+private:
+	void CalculateHeightMap_GameThread();
+
+	void CalculateHeightMap_RenderThread(FRHICommandList& RHICmdList, ERHIFeatureLevel::Type featureLevel, FRHITexture* outputRT, int32 surfaceSize, const FOceanInfo& oceanInfo);
+
+private:
+	FTexture2DRHIRef OutputTexture;
+	FUnorderedAccessViewRHIRef OutputTextureUAV;
+
 };
