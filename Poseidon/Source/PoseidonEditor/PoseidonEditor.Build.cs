e50a50d151dd48770ae40b6b7d1fe272520a42b6
YohiroW <yohiro36@foxmail.com>
2021-09-15 22:33:01 +0800

    Added editor support

---
+++ Poseidon/Source/PoseidonEditor/PoseidonEditor.Build.cs
@@ -0,0 +1,80 @@
+using UnrealBuildTool;
+using System;
+using System.IO;
+
+public class PoseidonEditor : ModuleRules
+{
+    public PoseidonEditor( ReadOnlyTargetRules Target ) : base( Target )
+    {
+        bPrecompile = true;
+        PCHUsage = PCHUsageMode.NoSharedPCHs;
+        PrivatePCHHeaderFile = "Private/PoseidonEditorPrivatePCH.h";
+
+        PublicIncludePaths.AddRange(
+            new string[] {
+                Path.Combine(ModuleDirectory, "Public")
+            }
+        );
+
+        PrivateIncludePaths.AddRange(
+            new string[] {
+                "Poseidon/Private",
+            }
+        );
+
+        PrivateIncludePathModuleNames.AddRange(
+            new string[] {
+                "PlacementMode"
+            }
+        );
+
+        // Add common dependencies.
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "CoreUObject",
+                "Poseidon",
+                "Slate",
+                "SlateCore",
+                "Landscape",
+            }
+        );
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "AppFramework",
+                "AssetTools",
+                "ContentBrowser",
+                "DesktopWidgets",
+                "EditorStyle",
+                "EditorWidgets",
+                "Engine",
+                "InputCore",
+                "LevelEditor",
+                "MainFrame",
+                "Projects",
+                "PropertyEditor",
+                "RHI",
+                "RawMesh",
+                "RenderCore",
+                "TargetPlatform",
+                "UnrealEd",
+                "ApplicationCore",
+                "CurveEditor",
+                "Json",
+                "SceneOutliner",
+                "PropertyPath",
+                "MaterialEditor"
+            }
+        );
+
+        DynamicallyLoadedModuleNames.AddRange(
+            new string[]
+            {
+                "PlacementMode",
+            }
+        );
+    }
+}
