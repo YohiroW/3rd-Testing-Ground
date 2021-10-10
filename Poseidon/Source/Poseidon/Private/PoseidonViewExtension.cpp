88619a58c7fc4d8483dde8891e962c6be38d9661
YohiroW <yohiro36@foxmail.com>
2021-10-10 23:47:39 +0800

    Updated PoseidonViewExtension

--- Poseidon/Source/Poseidon/Private/PoseidonViewExtension.cpp
+++ Poseidon/Source/Poseidon/Private/PoseidonViewExtension.cpp
@@ -13,11 +13,48 @@ FPoseidonViewExtension::~FPoseidonViewExtension()
 
 void FPoseidonViewExtension::SetupViewFamily(FSceneViewFamily& InViewFamily)
 {
+	if (bRebuildGPUData)
+	{
+		TResourceArray<FVector4> PoseidonIndirectionBuffer;
+		TResourceArray<FVector4> PoseidonDataBuffer;
 
+		if (PoseidonIndirectionBuffer.Num() == 0)
+		{
+			PoseidonIndirectionBuffer.AddZeroed();
+		}
+
+		if (PoseidonDataBuffer.Num() == 0)
+		{
+			PoseidonDataBuffer.AddZeroed();
+		}
+
+		ENQUEUE_RENDER_COMMAND(AllocatePoseidonInstanceDataBuffer)
+		(
+			[this, PoseidonDataBuffer, PoseidonIndirectionBuffer](FRHICommandListImmediate& RHICmdList) mutable
+			{
+				FRHIResourceCreateInfo CreateInfoData;
+				CreateInfoData.ResourceArray = &PoseidonDataBuffer;
+				CreateInfoData.DebugName = TEXT("PoseidonDataBuffer");
+				DataBuffer = RHICreateStructuredBuffer(sizeof(FVector4), PoseidonDataBuffer.GetResourceDataSize(), BUF_StructuredBuffer | BUF_ShaderResource | BUF_Static, ERHIAccess::SRVMask, CreateInfoData);
+				DataSRV = RHICreateShaderResourceView(DataBuffer);
+
+				FRHIResourceCreateInfo CreateInfoIndirection;
+				CreateInfoIndirection.ResourceArray = &PoseidonIndirectionBuffer;
+				CreateInfoIndirection.DebugName = TEXT("PoseidonIndirectionBuffer");
+				IndirectionBuffer = RHICreateStructuredBuffer(sizeof(FVector4), PoseidonIndirectionBuffer.GetResourceDataSize(), BUF_StructuredBuffer | BUF_ShaderResource | BUF_Static, ERHIAccess::SRVMask, CreateInfoIndirection);
+				IndirectionSRV = RHICreateShaderResourceView(IndirectionBuffer);
+			}
+		);
+
+		bRebuildGPUData = false;
+	}
 }
 
 void FPoseidonViewExtension::PreRenderView_RenderThread(FRHICommandListImmediate& RHICmdList, FSceneView& InView)
 {
+	if (DataSRV && IndirectionSRV)
+	{
 
+	}
 }
 
